90016b437a9ddc43e388b89e716b3a1bc198a355
James Saryerwinnie <jls.npi@gmail.com>
2011-11-13 14:28:59 -0800

    Add a setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='semidbm',
+    version='0.1.0',
+    description="An alternative to python's dumbdbm",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read(),
+    license='BSD',
+    author='James Saryerwinnie',
+    author_email='jlsnpi@gmail.com',
+    py_modules=['semidbm'],
+    zip_safe=False,
+    keywords="semidbm",
+    url="https://github.com/jamesls/semidbm",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
+
